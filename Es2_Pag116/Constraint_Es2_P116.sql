628748e90ded79bd576d85c18b6b7a996170d383
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-11-08 09:58:37 +0100

    Delete Es2_Pag116 directory

--- Es2_Pag116/Constraint_Es2_P116.sql
+++
@@ -1,3 +0,0 @@
-alter table libri 	
-	change column cognome autore_cognome varchar(50) not null,
-  	change column nome autore_nome varchar(50) not null;
--- Es2_Pag116/ModificaLibri_Es2_P116.sql
+++
@@ -1,5 +0,0 @@
-alter table libri
-  change column anno anno_pubblicazione int,
-  add column cognome varchar(40),
-  add column nome varchar(40),
-  drop column autore;
