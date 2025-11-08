25d62dba768997b0dfe8d6eb278f4821b01e2f19
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-11-08 09:57:27 +0100

    Delete Scuola directory

--- Scuola/AddValues.sql
+++
@@ -1,4 +0,0 @@
-insert into studenti (Nome, Cognome, DataNascita, Corso, Voto)
-values('Giuseppe', 'Bianchi', '2000-03-01', 'Informatica', 28),
-('Giovanni', 'Rossi', '2002-04-01', 'Elettronica', 25),
-('Nigga', 'Poli', '2000-03-01', 'Sistemi', 28);
--- Scuola/CreatTab.sql
+++
@@ -1,8 +0,0 @@
-create table Studenti (
- id_studente int auto_increment primary key, 
- Nome varchar(50),
- Cognome varchar(50),
- DataNascita date,
- Corso varchar(20),
- Voto decimal(4,2)
- );
