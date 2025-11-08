c9de2e8f7654c63dd9a16bb816572c999dd79075
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-11-08 09:58:50 +0100

    Delete Es4_Pag116 directory

--- Es4_Pag116/TabellaAlunni_Es4_P116.sql
+++
@@ -1,7 +0,0 @@
-create table autori (
-    codice int AUTO_INCREMENT PRIMARY KEY,
-    cf varchar(50) NOT NULL UNIQUE,
-    cognome varchar(50) NOT NULL,
-    nome varchar(50) NOT NULL,
-    citta varchar(50)
-);
