1801e991b50de053eebd42e1f968cbf22a55077f
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-11-08 10:06:55 +0100

    Delete SQL/Es4_Pag116 directory

--- SQL/Es4_Pag116/ModificaAlunni_Es4_P116.sql
+++
@@ -1,5 +0,0 @@
-alter table alunni
-	change column cf codice_fiscale int not null unique,
-	change column data data_nascita date,
-	add column corso varchar(20),
-	add column sezione varchar(5);
--- SQL/Es4_Pag116/RinominaCorsoTelefono_Es4_P116.sql
+++
@@ -1,3 +0,0 @@
-alter table alunni
-  change column corso indirizzo varchar(50),
-  drop column telefono;
