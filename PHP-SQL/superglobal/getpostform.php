51bd5d28186c289a8ec0c83c8716d99460b9aafc
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2026-05-08 11:47:37 +0200

    Add files via upload

--- PHP-SQL/superglobal/getpostform.php
+++ PHP-SQL/superglobal/getpostform.php
@@ -7,10 +7,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Prova</title>
 </head>
 <body>
-    <form method="GET" action = "esito.php">
+    <form method="POST" action = "esito.php">
 
         Nome: <br>
         <input type="text" name="nome"><br><br>
